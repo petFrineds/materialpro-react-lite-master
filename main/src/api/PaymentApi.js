a58c90d689e9bdd65be053d238d9ebbf335d43dd
SOYA95 <66457249+SOYA95@users.noreply.github.com>
2022-08-19 14:07:51 +0900

    api수정

--- main/src/api/PaymentApi.js
+++ main/src/api/PaymentApi.js
@@ -13,7 +13,7 @@ export const requestPayment = async params => {
   }
 };
 export const getAllPaymentHistory = async userId => {
-  const response = await PetFrinedsGetService('/payments/users/' + userId);
+  const response = await PetFrinedsGetService('/payments/pays/' + userId);
   if (response && response.status === 200) {
     console.log({ status: response.status, data: response.data });
     return { status: response.status, data: response.data };
